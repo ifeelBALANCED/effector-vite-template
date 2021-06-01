9bb0f082ad5eb4b1474306923672b12cc65de466
vlad <vladislavherasymenko@gmail.com>
2021-06-01 11:33:55 +0300

    fix: update .babelrc and vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import {babel} from "@rollup/plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()]
+    plugins: [
+        reactRefresh(),
+        babel({
+            include: ['./src/**'],
+            extensions: ['.ts', '.tsx'],
+            babelHelpers: 'bundled',
+        }),
+    ],
 })
