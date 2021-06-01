49804b9eb8454d64c758e98f6dbedc136f973d40
vlad <vladislavherasymenko@gmail.com>
2021-06-01 10:32:38 +0300

    fix: add babel-plugin to vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {babel} from '@rollup/plugin-babel'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    plugins: [reactRefresh(), {
+        ...babel(),
+        enforce: 'pre'
+    }]
 })
