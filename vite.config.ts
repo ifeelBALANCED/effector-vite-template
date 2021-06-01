c2258b2823e9e1657bfef8609f291db6e2d96d4b
vlad <vladislavherasymenko@gmail.com>
2021-06-01 11:11:06 +0300

    fix: remove babel from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import {defineConfig} from 'vite'
-import {babel} from '@rollup/plugin-babel'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh(), {
-        ...babel(),
-        enforce: 'pre'
-    }]
+    plugins: [reactRefresh()]
 })
